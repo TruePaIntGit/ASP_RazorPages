161dda3669e12ca36c3fa26bbb3e38c3868aedf8
TruePaInt <BOG@HOME>
2026-04-24 18:13:02 +0500

    class work

---
+++ RazorPages/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPages.Models
+{
+	public class Student
+	{
+		public int ID { get; set; }
+
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+
+		//Navigation properties
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
